3484834dd48ffe5483eff6cb8f97010b5b7295f8
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-05-01 16:13:38 +0100

    integrated leaflet API into Codebase
    - followed instructions on how to use leaflet.js
    - retrieved map access token
    - Successfully displays map

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//import {map} from "./map";
+
 async function getLocation (){
     const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_6LDGx376YFUU95TGL54y3ftV7SRae&ipAddress=129.205.124.230`)
     const data = await response.json();
     console.log(data.location);
 }
 getLocation();
+//console.log(map);
