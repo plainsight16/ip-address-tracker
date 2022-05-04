ba396e76ff6dd4138bfec4314c0a828a268dea31
plainsight16 <64688744+plainsight16@users.noreply.github.com>
2022-05-04 17:44:04 +0100

    linked both apis together

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ let mapLocation = {
         +ipAddress
         )
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => this.generateMap(data.location))
+    },
+
+    generateMap: function(data){
+        const {lat, lng} = data;
+        const map = L.map('map').setView([lat, lng], 13);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoicGxhaW5zaWdodDE2IiwiYSI6ImNsMm5lMmNhejE4ejkzZWwxaGxlNDRtbHIifQ.CBh2MlIjdPqe-Sx-gfBU0A'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map);
     }
 }
-console.log(mapLocation.getLocation("129.205.124.230"));
-
-
-const map = L.map('map').setView([51.05, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoicGxhaW5zaWdodDE2IiwiYSI6ImNsMm5lMmNhejE4ejkzZWwxaGxlNDRtbHIifQ.CBh2MlIjdPqe-Sx-gfBU0A'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map);
-console.log(map);
-//console.log(map);
+mapLocation.getLocation("129.205.124.230");
